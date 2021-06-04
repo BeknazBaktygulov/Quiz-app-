d3d8a822f17c3f729ab399ab123a18e425f689fa
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-04 13:36:14 -0700

    Developed states for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import QuestionCard from './QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
+function App () {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true);
 
 const startTrivia =  async () => {
 }
@@ -14,7 +23,7 @@ const nextQuestion = () => {
 
 } 
 
-function App () {
+
   return (
     <div className="App">
       <h1>React Quiz</h1>
@@ -25,7 +34,13 @@ function App () {
         Score:
       </p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard questionNum={number + 1}
+      totalQuestions={TOTAL_QUESTIONS}
+      questions={questions[number].questions}
+      answers={questions[number].answers}
+      userAnswers={ userAnswers ? userAnswers[number] : undefined }
+      callback={checkAnswer} />
+      
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
