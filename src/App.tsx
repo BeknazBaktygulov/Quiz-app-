d4b8c5baec1a1ed4af5afd8aac087948855c23da
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-04 13:10:32 -0700

    Created elements and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
+import QuestionCard from './QuestionCard';
 
-function App() {
+
+const startTrivia =  async () => {
+}
+
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+}
+
+const nextQuestion = () => {
+
+} 
+
+
   return (
     <div className="App">
-      <h1>Hello there</h1>
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">
+        Score:
+      </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
