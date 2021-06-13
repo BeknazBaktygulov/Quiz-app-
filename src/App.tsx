d3fd24bcd0905bc9e1417ce6568eedc3877207d0
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-13 13:46:09 -0700

    Defined startTrivia function to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,20 @@ function App () {
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY ))
+  console.log(questions)
 
 const startTrivia =  async () => {
+  setLoading(true);
+  setGameOver(false);
+  const newQuestions = await fetchQuizQuestions(
+    TOTAL_QUESTIONS,
+    Difficulty.EASY
+  );
+  setQuestions(newQuestions);
+  setScore(0);
+  setUserAnswers([])
+  setNumber(0);
+  setLoading(false);
 }
 
 const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
