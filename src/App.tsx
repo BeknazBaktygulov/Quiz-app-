525778a1bab2059f31f58bf76441d27fbb6ae7cb
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-13 14:11:36 -0700

    Added conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -55,14 +55,18 @@ const nextQuestion = () => {
       </button>
       ) : null }
       {!gameOver ?  <p className="score">Score:</p> : null }
-      {loading ? <p>Loading Questions...</p> : null }
-      {/* <QuestionCard questionNum={number + 1}
-      totalQuestions={TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={ userAnswers ? userAnswers[number] : undefined }
-      callback={checkAnswer} />
-  */}
+      {loading && <p>Loading Questions...</p>}
+      { !loading && !gameOver && ( 
+        <QuestionCard questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={ userAnswers ? userAnswers[number] : undefined }
+        callback={checkAnswer} />
+        )}
+      
+      
+  
       
       <button className="next" onClick={nextQuestion}>
         Next Question
